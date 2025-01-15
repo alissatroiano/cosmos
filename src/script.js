95f574c6ffbc88530fda91f655e37fd85c6f22f1
Alissa Troiano <alissatroianonyc@gmail.com>
2025-01-14 20:51:21 -0500

    Go back to having the player start game by clicking planet

--- src/script.js
+++ src/script.js
@@ -143,7 +143,7 @@ let gameOver = false;
 let playerAngle = 0;
 let enemyAngle = Math.PI; // Start moving enemy planet on opposite side
 let newEnemyAngle = Math.PI;
-let velocity = 0.025; // Initial speed for the player
+let velocity = 0.015; // Initial speed for the player
 const maxSpeed = 0.0425; // Maximum speed
 const minimumSpeed = 0.01245;
 const decelerationRate = 0.0125; // Deceleration rate per second
@@ -190,11 +190,7 @@ function animate() {
     playerPlanet.position.z = Math.sin(playerAngle) * trackRadius + centerAdjustZ;
 
     // move player planet at regular velocity if no acceleration or deceleration
-    if (velocity === 0) {
-        playerAngle += minimumSpeed;
-        playerPlanet.position.x = Math.cos(playerAngle) * trackRadius + centerAdjustX;
-        playerPlanet.position.z = Math.sin(playerAngle) * trackRadius + centerAdjustZ;
-    }
+
     // Track player loops and spawn enemy planets every 3 loops
     if (playerAngle >= Math.PI * 2) {
         playerAngle -= Math.PI * 2; // Reset angle after each loop
